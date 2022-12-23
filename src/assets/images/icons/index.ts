a8b44a4f9f85ba3348f0c0397bb60eb606b2391c
Pedro Silva <pedro.silva@cbyk.com.br>
2022-12-23 17:16:48 -0300

    fix: whatsapp icon import

--- src/assets/images/icons/index.ts
+++ src/assets/images/icons/index.ts
@@ -3,7 +3,7 @@ import Edit from './edit.png'
 import Mail from './mail.png'
 import Globe from './globe.png'
 import Phone from './phone.png'
-import WhatsApp from './whatsApp.png'
+import WhatsApp from './whatsapp.png'
 import Linkedin from './linkedin.png'
 
 export default {
